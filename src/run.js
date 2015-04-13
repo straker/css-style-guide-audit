44e97e0a493f0627dbad750b42ef2d9584327c1b
Steven Lambert <steven.lambert@ldschurch.org>
2015-04-13 15:18:34 -0600

    All changes to bookmarklet.js and run.js should be made on a local branch.

--- src/run.js
+++ src/run.js
@@ -1,7 +1,16 @@
 // Your code here
 document.addEventListener('styleSheetsParsed', function() {
   // allow custom rules to be audited
-  var auditRules = [];
+  var auditRules = [{
+    type: '',
+    selector: 'a[href^="javascript"], a[href="#"], a:not([href])',
+    description: 'Anchor tags that do not navigate should be buttons.'
+  },
+  {
+    type: '',
+    selector: '.fs-h1:not(h1):not(h2):not(h3):not(h4):not(h5), .fs-h2:not(h1):not(h2):not(h3):not(h4):not(h5), .fs-h3:not(h1):not(h2):not(h3):not(h4):not(h5), .fs-h4:not(h1):not(h2):not(h3):not(h4):not(h5), .fs-h5:not(h1):not(h2):not(h3):not(h4):not(h5)',
+    description: 'Style guide heading classes should not be applied to non-heading elements.'
+  }];
 
-  // auditStyleGuide('styleGuideSheet', ['ignoreSheet'], auditRules);
+  auditResults('familysearch-styles','hf', auditRules);
 });
